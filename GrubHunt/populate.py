f2fb755a6a852629534e19cb51fcf8e94abaf57f
Jocelyn <k2o0b@ugrad.cs.ubc.ca>
2015-10-23 20:36:07 -0700

    External data download added

--- GrubHunt/populate.py
+++ GrubHunt/populate.py
@@ -5,14 +5,36 @@ import django
 django.setup()
 
 from GrubHunt.models import FoodVendor
-#used for parsing lat and lon
-from decimal import Decimal
+from decimal import Decimal #used for parsing lat and lon
 import csv
 
+# used is the file is download from the original file source 
+import shutil
+import urllib2
+from contextlib import closing
+FILE_ADDRESS = 'ftp://webftp.vancouver.ca/OpenData/csv/street_food_vendors.csv'
+
+# used if the file is read from the static data
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 DATA_FILE = os.path.join(BASE_DIR, 'static/data/street_food_vendors.csv')
 
 class DatabasePopulater():
+
+    def populate(self):
+    
+        createdVendors = []
+        
+        #with open(DATA_FILE, "r") as myfile: 
+        with closing(urllib2.urlopen(FILE_ADDRESS)) as myfile:
+            reader = csv.reader(myfile)
+            next(reader, None)  # skip the headers
+            for row in reader:
+                values = self.parseLine(row)
+                created = self.add_vendor(values)
+                if created:
+            	    createdVendors.append(values[0])
+
+        return createdVendors
 
     def add_vendor(self, values):
         v, created = FoodVendor.objects.get_or_create(key=values[0])
@@ -37,17 +59,3 @@ class DatabasePopulater():
         else:
             row[4] = False
         return row
-
-    def populate(self):
-    
-        createdVendors = []
-        with open(DATA_FILE, "r") as myfile:
-            reader = csv.reader(myfile)
-            next(reader, None)  # skip the headers
-            for row in reader:
-                values = self.parseLine(row)
-                created = self.add_vendor(values)
-                if created:
-            	    createdVendors.append(values[0])
-
-        return createdVendors
